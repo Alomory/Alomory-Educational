7eb59f9651ab6e2c0c320a9c0d079623cf936a61
OMAR ISMAIL ABDJALEEL ALOMORY <s63955@ocean.umt.edu.my>
2023-11-05 00:05:53 +0800

    Task 5: task 1 and 2 was for task 3 and 4 respectively hehe. I have replaced the old fun with new GreetingText and added new Text in it.

--- s2324I/CSM3123-NATIVE_MOBILE_PROGRAMMIN/labs/Lab2/app/src/main/java/com/alomory/happybirthday/MainActivity.kt
+++ s2324I/CSM3123-NATIVE_MOBILE_PROGRAMMIN/labs/Lab2/app/src/main/java/com/alomory/happybirthday/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.alomory.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //TODO
                 }
             }
         }
@@ -30,10 +31,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, modifier: Modifier = Modifier){
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = message
     )
 }
 
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        Greeting("Omar Alomory")
+        GreetingText(message = "Happy Birthday Sam")
     }
 }
