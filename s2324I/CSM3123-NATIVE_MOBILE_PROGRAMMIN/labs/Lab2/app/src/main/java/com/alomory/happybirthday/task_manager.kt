b7996e5e544bfa90aaaee6b425f0ddf2ffd5790d
OMAR ISMAIL ABDJALEEL ALOMORY <s63955@ocean.umt.edu.my>
2023-11-07 01:49:13 +0800

    Updated task_manager.kt

--- s2324I/CSM3123-NATIVE_MOBILE_PROGRAMMIN/labs/Lab2/app/src/main/java/com/alomory/happybirthday/task_manager.kt
+++ s2324I/CSM3123-NATIVE_MOBILE_PROGRAMMIN/labs/Lab2/app/src/main/java/com/alomory/happybirthday/task_manager.kt
@@ -43,55 +43,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   ComposeQuadrantApp()
-                }
-            }
-        }
-    }
-}
-
-package com.alomory.happybirthday
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.alomory.happybirthday.ui.theme.HappyBirthDayTheme
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            HappyBirthDayTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
                    TaskCompletedScreen()
                 }
             }
