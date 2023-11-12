597ca30da97233bfcda2ad66c4c02cb0959c1502
OMAR ISMAIL ABDJALEEL ALOMORY <s63955@ocean.umt.edu.my>
2023-11-13 03:37:39 +0800

    removed the unnecessarily libraries
    there was unnecessary libraries added to the files, so i just removed it and make the code look cleaner.

--- s2324I/CSM3123-NATIVE_MOBILE_PROGRAMMIN/labs/Lab3/Kotlin_fundamentals/app/src/main/java/com/alomory/diceroller/MainActivity.kt
+++ s2324I/CSM3123-NATIVE_MOBILE_PROGRAMMIN/labs/Lab3/Kotlin_fundamentals/app/src/main/java/com/alomory/diceroller/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.alomory.diceroller.R
 import com.alomory.diceroller.ui.theme.DiceRollerTheme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Spacer
--- s2324I/CSM3123-NATIVE_MOBILE_PROGRAMMIN/labs/Lab3/Lemonade/app/src/main/java/com/alomory/lemonade/MainActivity.kt
+++ s2324I/CSM3123-NATIVE_MOBILE_PROGRAMMIN/labs/Lab3/Lemonade/app/src/main/java/com/alomory/lemonade/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -37,7 +35,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.alomory.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
