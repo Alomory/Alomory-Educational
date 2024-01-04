ce5373d6d3265730c64c335f948e12c0b9c2a62b
OMAR ISMAIL ABDJALEEL ALOMORY <s63955@ocean.umt.edu.my>
2024-01-04 14:17:26 +0800

    updated some comments

--- s2324I/CSM3123-NATIVE_MOBILE_PROGRAMMIN/labs/LabTestToGitHub/main/java/com/alomory/task1/MainActivity.java
+++ s2324I/CSM3123-NATIVE_MOBILE_PROGRAMMIN/labs/LabTestToGitHub/main/java/com/alomory/task1/MainActivity.java
@@ -22,8 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // the Bonus mark display appbar 
+        // The bonus marks here i guess 
         setSupportActionBar(findViewById(R.id.my_toolbar));
         // Initialize views
         textView4 = findViewById(R.id.textView4);
